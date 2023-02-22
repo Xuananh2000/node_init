87adf701a1b2720cc17fc4877764d1748d938662
Admin <xuanna@kaiyouit.com>
2023-02-22 23:20:26 +0700

    [Add]-Add Boostrap, SCSS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require('path')
 const { engine } = require ('express-handlebars')
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public/')))
+
 app.use(morgan('combined'))
 
 // app.engine('handlebars', engine())
@@ -17,6 +19,14 @@ app.set('views', path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
 });
 
 app.listen(port, () => {
